bbed2994443f5c73ad263bdcfabf37ac63ac26f9
unknown <anasamohammad.school@gmail.com>
2023-12-06 05:29:23 +0000

    Have added print statements and damage dealing also pseudo round based combat

---
+++ javaStuff/FOP-Valley/src/fop/valley/Game.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fop.valley;
+
+/**
+ *
+ * @author Dummfak
+ */
+public class Game {
+    
+    public static void gameOver() {
+        System.out.println("You have died");
+        System.out.println("Try again?");
+    }
+    
+}
