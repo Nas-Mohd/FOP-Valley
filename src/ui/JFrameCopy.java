3c9bb62de9047ef7e85a186e085a61657550004b
Nas <anasamohammad.school@gmail.com>
2023-12-17 16:51:48 +0800

    trying to commit

---
+++ src/ui/JFrameCopy.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Anas Mohammad 23055727
+ */
+public class JFrameCopy {
+    
+    private JFrame frame;
+    
+    public JFrameCopy() {
+        initialize();
+    }
+    
+    public void initialize() {
+        frame = new JFrame();
+        this.frame.setTitle("UM Game");
+        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setSize()
+        
+    }
+    
+}
