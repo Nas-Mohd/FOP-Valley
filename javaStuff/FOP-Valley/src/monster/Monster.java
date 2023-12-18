ee41ef6315de4b1569d9880be16a5e721ee200f6
chris <chris@Christine>
2023-12-18 11:41:23 +0800

    test

---
+++ javaStuff/FOP-Valley/src/monster/Monster.java
@@ -0,0 +1,32 @@
+package monster;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author chris
+ */
+public class Monster {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        
+        public String name;
+        public int healthPoints;
+        public int manaPoints;
+        public int Attack;
+        public int Defense;
+        public int credit;
+        protected EncounterLogs encounterLogs;
+    
+    
+     public Monster(){
+     encounterLogs = new EncounterLogs();
+        }
+}
+
